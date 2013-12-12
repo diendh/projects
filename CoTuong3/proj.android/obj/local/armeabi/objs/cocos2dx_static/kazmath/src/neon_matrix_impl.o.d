944b2800d6b4827eb4b358c51de1a58d4c313ab7
diendh <diendh001@gmail.com>
2013-12-12 11:41:39 +0700

    Revert 0d1aa74..62db4c2
    This rolls back to commit 0d1aa749cd0b88dd5623418d63d4c52ed1ec6fa1.

--- CoTuong3/proj.android/obj/local/armeabi/objs/cocos2dx_static/kazmath/src/neon_matrix_impl.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/kazmath/src/neon_matrix_impl.o: \
- /Volumes/DATA/Android/cocos2d-x-2.2.1/projects/CoTuong3/proj.android/../../../cocos2dx/kazmath/src/neon_matrix_impl.c \
- /Volumes/DATA/Android/cocos2d-x-2.2.1/projects/CoTuong3/proj.android/../../../cocos2dx/kazmath/include/kazmath/neon_matrix_impl.h
-
-/Volumes/DATA/Android/cocos2d-x-2.2.1/projects/CoTuong3/proj.android/../../../cocos2dx/kazmath/include/kazmath/neon_matrix_impl.h:
